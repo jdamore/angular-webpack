e8edf4ff4324652aae387f87c2c03244d7af98f3
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-05-31 21:17:31 +0800

    Not mangling everything.

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,9 @@ module.exports = {
       compress: {
         warning: false,
       },
+      mangle: {
+        except: ['$super', '$', 'exports', 'require',]
+      },
     }),
   ],
 };
--- webpack.config.test.js
+++ webpack.config.test.js
@@ -2,8 +2,8 @@ module.exports = {
   target: 'node',
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.html$/, exclude: /node_modules/, loader: 'html-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', },
+      { test: /\.html$/, exclude: /node_modules/, loader: 'html-loader', },
     ],
   },
 };
